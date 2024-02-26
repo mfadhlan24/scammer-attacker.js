0fc43a56ff429a97cf9f1605cae681ed7598834c
mfahdlan24 <m.fadhlan24@outlook.com>
2024-02-26 23:37:32 +0700

    add : basic

--- mampus.js
+++ mampus.js
@@ -1,13 +1,17 @@
 const {default: axios} = require("axios")
 
 
-const pesannya = `Dasar Penipu Dongooooooooooooooooooo!`
-const url = `https://api.telegram.org/bot7086188753:AAHjCA_82Oi4v1LYJ-sP-v-7WQx-KZffY6A/sendMessage?parse_mode=markdown&chat_id=6361732536&text=${pesannya.split(" ").join("%20")}`
+let pesannya = `Dasar Penipu Dongooooooooooooooooooo!` // isi pesan yang akan dikirim
+pesannya = pesannya.split(" ").join("%20")
+const url = `https://api.telegram.org/bot7086188753:AAHjCA_82Oi4v1LYJ-sP-v-7WQx-KZffY6A/sendMessage?parse_mode=markdown&chat_id=6361732536&text=` 
 
+// isi urlnya di atas dengan url bot telegram yang sudah di dapatkan
+
+const [baseURL, textParam] = url.split('&text=');
 async function sendMessage() {
     try {
         while (true) {
-            await axios.get(url)
+            await axios.get(baseURL+"&text="+pesannya)
                 .then((response) => {
                     if (response.status == 200) {
                         console.log(response)
